20635dc36acf08ecaac41c00365f3aa793a0b2a7
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-07-16 16:03:09 -0400

    fix: workflow

--- src/worker.py
+++ src/worker.py
@@ -12,7 +12,7 @@ hatchet = Hatchet(debug=True)
 @hatchet.workflow(on_events=["send:text"])
 class TextMom:
     @hatchet.step(timeout="10s")
-    def generate_message(self):
+    def generate_message(self, context):
         print(
             "starting step1",
             time.strftime("%H:%M:%S", time.localtime()),
@@ -21,40 +21,14 @@ class TextMom:
             "step1": "done",
         }
 
-    @hatchet.step()
-    def send_message(self):
+    @hatchet.step(parents=["generate_message"], timeout="10s")
+    def send_message(self, context):
         print(
             "starting step2",
             time.strftime("%H:%M:%S", time.localtime()),
         )
         return {
             "step2": "step2",
-        }
-
-    @hatchet.step(parents=["step1", "step2"])
-    def step3(self, context: Context):
-        print(
-            "executed step3",
-            time.strftime("%H:%M:%S", time.localtime()),
-            context.workflow_input(),
-            context.step_output("step1"),
-            context.step_output("step2"),
-        )
-        return {
-            "step3": "step3",
-        }
-
-    @hatchet.step(parents=["step1", "step3"])
-    def step4(self, context: Context):
-        print(
-            "executed step4",
-            time.strftime("%H:%M:%S", time.localtime()),
-            context.workflow_input(),
-            context.step_output("step1"),
-            context.step_output("step3"),
-        )
-        return {
-            "step4": "step4",
         }
 
 def main():
